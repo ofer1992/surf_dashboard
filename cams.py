ce19c4352058593075001c52ccfef3ca913f3fac
ofer1992 <geon.arcan@gmail.com>
2023-04-22 13:29:38 +0300

    organized cams

--- cams.py
+++ cams.py
@@ -1,33 +1,61 @@
 from bs4 import BeautifulSoup
 import json
 
-html = """<head>
+def generate(html, relevant_cams, fname):
+    page = BeautifulSoup(html, "html.parser")
+    with open("stream_url.json", 'r') as f:
+        stream_urls = json.load(f)
+
+    # streamitup cams
+    stream_urls = {k:v for k,v in stream_urls.items() if k in relevant_cams}
+    for k, v in stream_urls.items():
+        grid_item = page.new_tag("div", attrs={"class":"grid-item"})
+        vid = page.new_tag("video", id=k+"-cam", width="500", height="500", controls="", preload="auto", attrs={"class":"video-js vjs-default-skin"})
+        src = page.new_tag("source", src=v, type="application/x-mpegURL")
+
+        page.body.div.append(grid_item)
+        grid_item.append(vid)
+        vid.append(src)
+
+    script = page.new_tag("script")
+    script.string = " ".join([f"videojs('{k}-cam').play();" for k in stream_urls.keys()])
+    page.body.append(script)
+
+    with open(fname, 'w') as f:
+        f.write(page.prettify())
+
+html_tlv = """<head>
     <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
     <script src="https://unpkg.com/video.js/dist/video.js"></script>
 </head>
 
 <body>
     <div class="grid-container">
-
+        <div class="grid-item">
+            <iframe id="cam" width="500" height="500" allowfullscreen
+                src="https://gocamstream.com/WebRTCApp/play.html?name=0002" scrolling="no">
+            </iframe>
+        </div>
     </div>
 </body>"""
 
-page = BeautifulSoup(html, "html.parser")
-with open("stream_url.json", 'r') as f:
-    stream_urls = json.load(f)
+html_haifa = """<head>
+    <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
+    <script src="https://unpkg.com/video.js/dist/video.js"></script>
+</head>
 
-for k, v in stream_urls.items():
-  grid_item = page.new_tag("div", attrs={"class":"grid-item"})
-  vid = page.new_tag("video", id=k+"-cam", width="500", height="500", controls="", preload="auto", attrs={"class":"video-js vjs-default-skin"})
-  src = page.new_tag("source", src=v, type="application/x-mpegURL")
-  
-  page.body.div.append(grid_item)
-  grid_item.append(vid)
-  vid.append(src)
+<body>
+    <div class="grid-container">
+        <div class="grid-item">
+            <iframe src="https://g2.ipcamlive.com/player/player.php?alias=5ffd9eb29b665" width="500" height="500"
+                    autoplay allowfullscreen></iframe>
+        </div>
+        <div class="grid-item">
+            <iframe src="https://g0.ipcamlive.com/player/player.php?alias=60acaa1aeee83" width="500" height="500"
+                autoplay allowfullscreen></iframe>
+        </div>
+    </div>
+</body>"""
 
-script = page.new_tag("script")
-script.string = " ".join([f"videojs('{k}-cam').play();" for k in stream_urls.keys()])
-page.body.append(script)
-
-with open("cams.html", 'w') as f:
-   f.write(page.prettify())
+generate(html_tlv, ["dolphinarium", "hilton"], "cams_haifa.html")
+generate(html_haifa, ["bat-galim", "meridian"], "cams_haifa.html")
--- fetch_stream.py
+++ fetch_stream.py
@@ -6,7 +6,8 @@ import json
 
 urls = {"dolphinarium": "https://beachcam.co.il/dolfinarium.html",
         "hilton": "https://beachcam.co.il/yamit.html",
-        "bat-galim": "https://beachcam.co.il/batgalim.html"}
+        "bat-galim": "https://beachcam.co.il/batgalim.html",
+        "meridian": "https://beachcam.co.il/meridian.html"}
 stream_urls = {}
 for name, url in urls.items():
     response = requests.get(url)
